c0d497bbfa922f256d793ab1113068c244649656
Alexey Rodimov <alexey.rodimov@gmail.com>
2022-02-13 14:04:31 +0300

    Add rendering to html with jinja2

--- main.py
+++ main.py
@@ -12,10 +12,13 @@ import feedparser
 import spacy
 from sklearn.cluster import SpectralClustering
 
+# render to html
+from jinja2 import Environment, FileSystemLoader
+import os
+
 # wordcloud display
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
-
 
 # currying lib functions for composition
 walk = curry(walk)
@@ -28,7 +31,6 @@ lcat = curry(lcat)
 
 # load russian language model
 nlp = spacy.load("ru_core_news_md")
-
 
 # List of feeds URLs -> List of Entries
 load_rss = compose(
@@ -56,7 +58,7 @@ def add_keywords(entry):
         ' '.join,
         distinct,
         lpluck_attr('lemma_'),
-        lfilter(lambda t: not(t.lemma_ in custom_stop_words)),
+        lfilter(lambda t: not (t.lemma_ in custom_stop_words)),
         lfilter(lambda t: t.pos_ in ['PROPN', 'NOUN', 'VERB']),
         lfilter(lambda t: not (t.is_stop | t.is_punct)),
         lambda doc: [token for token in doc],
@@ -70,10 +72,10 @@ def add_keywords(entry):
 @log_enters(print)
 def clusterize(entries):
     clusters = compose(
-        SpectralClustering(75).fit_predict,
+        SpectralClustering(150).fit_predict,
         lambda xs: [[x1.similarity(x2) for x1 in xs] for x2 in xs],
-        walk(nlp),              # TODO optimise
-        lpluck('keywords')      # TODO remove double call to lpluck
+        walk(nlp),  # TODO optimise
+        lpluck('keywords')  # TODO remove double call to lpluck
     )
     return [dict(d, **{'cluster': v}) for v, d in zip(clusters(entries), entries)]
 
@@ -92,7 +94,7 @@ def top_words(entries):
 # Entry -> String
 def prettify(entry):
     return fg.blue + '[' + str(entry['cluster']) + '] ' + fg.rs + \
-        ef.b + entry['title'] + ef.rs
+           ef.b + entry['title'] + ef.rs
     # + '\n' + ef.dim + fill(entry['summary']) + ef.dim
     # + '\n' + '[' + ef.dim + entry['keywords'] + ef.dim + ']'
 
@@ -110,6 +112,7 @@ print_clusters = compose(
     lambda d: d.values(),
     group_by(lambda x: x['cluster']),
 )
+
 
 def show_word_cloud(entries):
     text = compose(
@@ -129,6 +132,21 @@ def show_word_cloud(entries):
     plt.show()
 
 
+def render_html(entries):
+    clusters = group_by(lambda x: x['cluster'])(entries)
+
+    root = os.path.dirname(os.path.abspath(__file__))
+    templates_dir = os.path.join(root, 'templates')
+    env = Environment(loader=FileSystemLoader(templates_dir))
+    template = env.get_template('index.html')
+    filename = os.path.join(root, 'output', 'index.html')
+
+    with open(filename, 'w') as fh:
+        fh.write(template.render(
+            clusters=clusters,
+        ))
+
+
 def main():
     feeds = [
         'https://lenta.ru/rss/news',
@@ -140,12 +158,13 @@ def main():
     clustered_feeds = compose(
         clusterize,
         walk(add_keywords),
-        select(['title']),
+        select(['title', 'link']),
         load_rss
     )(feeds)
     print(print_clusters(clustered_feeds))
-    print(top_words(clustered_feeds))
-    show_word_cloud(clustered_feeds)
+    #print(top_words(clustered_feeds))
+    #show_word_cloud(clustered_feeds)
+    render_html(clustered_feeds)
 
 
 if __name__ == '__main__':
